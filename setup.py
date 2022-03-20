058d0c3aeab5996d976fb4963df0de103b24a712
wizyoung <happyyanghehe@gmail.com>
2022-03-21 01:42:40 +0800

    change version format

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cypy',
-      version='2022.3.16.a',
+      version='20220321.a',
       description="wizyoung's personal python utilities",
       classifiers=[
         'Programming Language :: Python',
